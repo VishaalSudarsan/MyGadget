cfc933530345a74c7019bb70aa77eefbb2539ec9
Vishaal Sudarsan <73996658+VishaalSudarsan@users.noreply.github.com>
2021-05-14 21:58:04 +0530

    Add files via upload

--- MyGadget.js
+++ MyGadget.js
@@ -1,6 +1,9 @@
 ﻿function Show()
 {
 var2 = new Date();
-document.getElementById('DateTime').innerHTML = var2;
+document.getElementById('Time').innerHTML = moment(var2).format("HH:mm:ss:SS");
+document.getElementById('Date').innerHTML = moment(var2).format("dddd, MMMM Do YYYY");
+document.getElementById('Week').innerHTML = moment(var2).format("Wo [Week of the Year]");
+//document.getElementById('Time').innerHTML = moment(var2).format("HH:mm:ss:SS");
 }
-setInterval(Show, 1000);
+setInterval(Show, 10);
